e6ae74723a977050d04f1104c1f7c9c091efd57e
rilesc555 <rilesconnors555@gmail.com>
2024-07-21 12:51:38 -0500

    fix sidebar width

--- build/js/script.js
+++ build/js/script.js
@@ -11,6 +11,10 @@ $(document).ready(function () {
     $("#btn_resume").click(function () {
       window.location.assign("./resume.pdf");
     })
+  $("#btn_home").click(function() {
+    $("#bio").fadeIn("fast", function () {});  })
+  $("#btn_home").click(function() {
+    $("#education").hide();  })
   $(".BTN_CLOSE").click(function () {
     $(this).closest(".window").fadeOut("fast", function () { });
   });
