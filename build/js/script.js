eb7924599c0d417a164d036be5ef631f89c1edb0
rilesc555 <rilesconnors555@gmail.com>
2024-07-25 20:59:51 -0500

    almost done

--- build/js/script.js
+++ build/js/script.js
@@ -1,20 +1,28 @@
 $(document).ready(function () {
   $("#btn_bio").click(function () { 
-    $("#bio").fadeIn("fast", function () { }); });
-  $("#btn_bio").click(function () { 
-    $("#education").hide(); });
+    $("#bio").fadeIn("fast", function () { }); 
+    $("#education").hide(); 
+    $("#contact").hide(); });
+
   $("#btn_education").click(function () { 
-    $("#education").fadeIn("fast", function () { }); });
-  $("#btn_education").click(function () { 
-    $("#bio").hide(); });  
+    $("#education").fadeIn("fast", function () { }); 
+    $("#bio").hide();
+    $("#contact").hide(); });
   //this function uses button to link to resume pdf
     $("#btn_resume").click(function () {
       window.location.assign("./resume.pdf");
     })
+  //this function shows contact info
+  $("#btn_contact").click(function () {
+    $("#contact").fadeIn("fast", function () { });
+    $("#bio").hide();
+    $("#education").hide();
+  })
+  //this function hides everything except contact info
   $("#btn_home").click(function() {
-    $("#bio").fadeIn("fast", function () {});  })
-  $("#btn_home").click(function() {
-    $("#education").hide();  })
+    $("#bio").fadeIn("fast", function () {});  
+    $("#education").hide();
+    $("#contact").hide(); })
   $(".BTN_CLOSE").click(function () {
     $(this).closest(".window").fadeOut("fast", function () { });
   });
